8b232d16441b382ea8ac9a7aca07720583cc281c
podstanar <stxalex@gmail.com>
2021-12-05 11:03:09 +0100

    pushed day3 solutions

---
+++ day3/prob1.py
@@ -0,0 +1,29 @@
+def list_to_bin(bList):
+    binNum = 0
+    for bit in bList:
+        binNum = binNum * 2 + bit
+    return binNum
+
+# ----------
+with open('input.txt', 'r') as f:
+    fullList = f.read().splitlines()
+
+bitlist = [[int(bit) for bit in line] for line in fullList]
+
+result = []
+ones = zeros = 0
+for i in range(len(bitlist[0])):
+    for line in bitlist:
+        if line[i] == 1:
+            ones += 1
+        else: # 0
+            zeros += 1
+    result.append(1 if ones > zeros else 0)
+    ones = zeros = 0
+
+gamma = list_to_bin(result)
+epsilon = int(''.join(['1' if i == '0' else '0' for i in bin(gamma)[2:]]), 2)
+
+print('\ngamma rate:', gamma)
+print('epsilon rate:', epsilon)
+print('\npotrošnja:', gamma * epsilon)
---
+++ day3/prob2.py
@@ -0,0 +1,43 @@
+def list_to_bin(bList):
+    binNum = 0
+    for bit in bList:
+        binNum = binNum * 2 + bit
+    return binNum
+
+def fetch_report(someList, ratingType, row):
+    onesList = []
+    zerosList = []
+    for line in someList:
+        if line[row] == 1:
+            onesList.append(line)
+        else: # 0
+            zerosList.append(line)
+    
+    calcOxy = len(onesList) >= len(zerosList)
+    if ratingType == 'oxygen':
+        return onesList if calcOxy else zerosList
+    else: # CO2
+        return onesList if not calcOxy else zerosList
+
+# ----------
+with open('input.txt', 'r') as f:
+    fullList = f.read().splitlines()
+
+bitlist = [[int(bit) for bit in line] for line in fullList]
+
+i = 0
+oxygen = fetch_report(bitlist, 'oxygen', i)
+co2 = fetch_report(bitlist, 'co2', i)
+while i in range(len(bitlist[0])):
+    i += 1
+    if len(oxygen) > 1:
+        oxygen = fetch_report(oxygen, 'oxygen', i)
+    if len(co2) > 1:
+        co2 = fetch_report(co2, 'co2', i)
+
+oxygen = list_to_bin(oxygen[0])
+co2 = list_to_bin(co2[0])
+
+print('\noxygen generator rating:', oxygen)
+print('CO2 scrubber rating:', co2)
+print('\nlife support rating:', oxygen * co2)
